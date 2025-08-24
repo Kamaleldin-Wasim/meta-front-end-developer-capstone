02b35e58c017f9d6f8f6993b3da694a27b0df129
Kamaleldin-Wasim <kimowasim123@gmail.com>
2025-08-24 23:22:17 +0300

    Add About component

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <h2>About Us</h2>
+                <p>We are a team of passionate food lovers...</p>
+                <p>Our mission is to bring the best culinary experiences to your doorstep.</p>
+                <p>Join us on this delicious journey!</p>
+                <div>
+                    <h3>Our Values</h3>
+                    <ul>
+                        <li>Quality Ingredients</li>
+                        <li>Exceptional Service</li>
+                        <li>Community Engagement</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Our Story</h3>
+                    <p>Founded in 2020, we started as a small food truck...</p>
+                </div>
+            </div>
+            <div>
+                <img src="https://via.placeholder.com/400" alt="About Us" />
+            </div>
+        </div>
+    );
+}
+
+export default About;
